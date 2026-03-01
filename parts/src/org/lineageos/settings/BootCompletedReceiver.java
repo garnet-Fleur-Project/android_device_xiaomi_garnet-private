5627568993f323dec3643eda5284c9252c4460c4
pabloescobar-reborn <yogivangala07@gmail.com>
2026-03-01 23:38:43 +0530

    garnet: parts: Remove unused Doze implementation and service
    This was never actually used since we rely on nonui and aod notifiers
    Better late than never drop the unused code
    
    Signed-off-by: pabloescobar-reborn <pabloescobar-reborn@users.noreply.github.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -32,7 +32,6 @@ import android.util.Log;
 import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
@@ -74,9 +73,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     private void startServices(Context context) {
         if (DEBUG) Log.i(TAG, "Starting services...");
-
-        // Initialize Doze features
-        DozeUtils.onBootCompleted(context);
 
         // Start Thermal Management Services
         ThermalUtils.getInstance(context).startService();
