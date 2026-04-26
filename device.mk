58f4967cb473c726ec59ceaaa23d0d444953aebf
Jotanh <jotanh@jotanic.xyz>
2026-04-26 02:00:45 +0000

    garnet: Implement torch light control
    [RealJohnGalt: Modify for oneplus usage and add toggle switch]
    [cyberknight777/ralph950412: Adapt nodes and SEPolicy for Xiaomi SM8350]
    [rk134: Adapt nodes/sepolicy for Xiaomi SM8450]
    [Jotanh: Adapt nodes/SEPolicy for Xiaomi SM7435]
    
    Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: rk134 <rahul@aospa.co>
    Signed-off-by: Jotanh <jotanh@jotanic.xyz>
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>

--- device.mk
+++ device.mk
@@ -123,6 +123,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor
 
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_sm7435)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
